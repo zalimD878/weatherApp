735442d8743671b104a39ba9dc3ff096b260e9a1
zalimD878 <z.dabagov@mail.ru>
2025-02-03 17:56:27 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import "./App.css";
+import { Input } from "./components/input";
+
+export default function App() {
+  return <Input handleSearch={() => {}} grade={7} />;
+}
---
+++ src/components/input.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+interface InputProps {
+  grade: number;
+  handleSearch: (text: string) => void;
+}
+
+export function Input({ grade, handleSearch }: InputProps) {
+  const [text, setText] = useState("");
+
+  function handleClick() {
+    console.log(grade);
+
+    handleSearch(text);
+  }
+
+  return (
+    <div>
+      <h1>Поиск</h1>
+
+      <div className="input">
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Введите ваш город"
+        />
+        <button className="button" onClick={handleClick}></button>
+      </div>
+    </div>
+  );
+}
