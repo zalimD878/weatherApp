3a5553a07824ac606482069d98da5b2e1544edbd
zalimD878 <z.dabagov@mail.ru>
2025-02-07 16:29:02 +0300

    weatherData

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,30 @@ import { getWeatherByCity } from "./api/weather";
 import "./App.css";
 import { CurrentWeather } from "./components/CurrentWeather";
 import { Input } from "./components/input";
-import { data } from "./Data";
 import { useState } from "react";
-
-const {
-  weather: { main, description },
-  main: { feels_like, pressure, sea_level, grnd_level, temp },
-  wind: { speed, deg },
-  clouds: { all },
-  sys: { country, sunrise, sunset },
-  timezone,
-  name,
-} = data;
+import { WeatherDataType } from "./types/weather";
 
 export default function App() {
-  const [name, setName] = useState("");
+  const [weatherData, setWeatherData] = useState<WeatherDataType | null>(null);
 
   const handleSearch = async (city: string) => {
     const data = await getWeatherByCity(city);
 
-    console.log(data);
-    setName(data.name);
-  };
-
-  const weatherData = {
-    description,
-    feels_like,
-    pressure,
-    speed,
-    deg,
-    name,
-    temp,
+    setWeatherData({
+      description: data.weather?.[0]?.description || "",
+      feels_like: data.main.feels_like,
+      pressure: data.main.pressure,
+      speed: data.wind.speed,
+      deg: data.wind.deg,
+      name: data.name,
+      temp: data.main.temp,
+    });
   };
 
   return (
     <div>
       <Input handleSearch={handleSearch} grade={7} />
-      <CurrentWeather weatherData={weatherData} />
+      {weatherData && <CurrentWeather weatherData={weatherData} />}
     </div>
   );
 }
