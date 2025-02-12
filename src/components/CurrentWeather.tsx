cafd211b031f7a362e3d2f4f8d7458fbf800d3f9
zalimD878 <z.dabagov@mail.ru>
2025-02-12 22:28:59 +0300

    weather-card-ch

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -1,6 +1,6 @@
 import { windDirectionConverter } from "../utils/windDirectionConverter";
 import barometr from "../assets/svg/barometr.svg";
-import windIcon from "../assets/svg/wind.svg";
+import windIcon from "../assets/svg/wind-svgrepo-com.svg";
 
 interface CurrentWeatherProps {
   weatherData: {
@@ -20,19 +20,22 @@ export function CurrentWeather({ weatherData }: CurrentWeatherProps) {
       <div className="weather-container">
         <p className="weather-city">{weatherData.name}</p>
         <div className="weather-main">
-          <div>
-            <p className="weather-temperature">{weatherData.temp}°</p>
-          </div>
+          <div className="temperature-discription-block">
+            <div>
+              <p className="weather-temperature">{weatherData.temp}°</p>
+            </div>
 
-          <div className="description-container">
-            <p className="weather-description">{weatherData.description}</p>
-            <p className="weather-feels-like">
-              ощущается как {weatherData.feels_like}
-            </p>
+            <div className="description-container">
+              <p className="weather-description">{weatherData.description}</p>
+              <p className="weather-feels-like">
+                ощущается как {weatherData.feels_like}
+              </p>
+            </div>
           </div>
-          <img src={barometr} alt="" />
-          <img src={windIcon} alt="" />
+          <img src={barometr} className="barometr-icon" />
+
           <p className="weather-pressure">{weatherData.pressure} мм рт. ст.</p>
+          <img src={windIcon} className="wind-icon" />
           <p className="weather-wind-speed">ветер {weatherData.speed} м/с, </p>
           <p className="weather-wind-direction">
             {windDirectionConverter(weatherData.deg)}
